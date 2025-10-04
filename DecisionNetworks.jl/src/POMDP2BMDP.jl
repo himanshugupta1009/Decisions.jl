b47a90748683490e43c896961ebaea307ca4cf37
oferdagan <ofer.dagan@colorado.edu>
2025-10-03 22:59:01 -0600

    Working RockSamplePOMDP to BMDP example

--- DecisionNetworks.jl/src/POMDP2BMDP.jl
+++ DecisionNetworks.jl/src/POMDP2BMDP.jl
@@ -8,104 +8,36 @@ using Random
 Pkg.activate(".")
 using POMDPs
 using POMDPTools
-
-
-myPOMDP = POMDP_DN(
-        # a = (; m) -> "action",
-        o = (; s, a) -> "obs",
-        r = (; s, a, sp) -> "rwd",
-        sp = (; s, a) -> "successor state"
-        )
-dnplot(myPOMDP)
-
-myPOMDP = RockSampleDecisions()
-dnplot(myPOMDP.model)
-# Step 1: 
-myBMDP = myPOMDP |> Unimplement(:r, :o,:mp)
-
-# Step 2: 
-myBMDP = myBMDP |> MergeForward(:s, :sp, :o)
-dnplot(myBMDP.model)
-
-
-# Step 3: define an updater from memory node m to memory node mp:
-# # 3.1 generate environment model graph:
-# myEnv = myPOMDP |> Unimplement(:r, :a,:mp)
-
-# myEnv = myEnv |> MergeForward(:m, :mp, :r)
-# dnplot(myEnv)
-
-
-function genObs(rng, m, a)
-    # Set s in the environment model to a sample from m, and :a to a.
-    s = rand(rng, m)
-
-    sp = rand(myPOMDP[:sp]; s, a)
-    o = rand(myPOMDP[:o]; s, a, sp)
-    
-    return o
-end
-
-function update( m, a, o)
-    # Set s in the environment model to a sample from m, and :a to a.
-    return m
-end
-
-mem_updater = @ConditionalDist Any begin
-    function rand(rng; m, a)
-        o = genObs(rng; m,a)
-        update(m, a, o)
-    end
-end
-myBMDP = myBMDP |> Implement(;
-    mp = mem_updater 
-)
-
-# Step 4: recondition r on m, a, mp
-myBMDP = myBMDP |> Recondition(; r =(Dense(:a),Dense(:m),Dense(:mp)))
-dnplot(myBMDP)
-
-
-# Step 5: Rename m--> s, and mp --> sp
-myBMDP = myBMDP |> Rename(; m = :s, mp = :sp)
-dnplot(myBMDP)
-
+using RockSample
+include("../../DecisionDomains.jl/src/rocksample.jl")
 
 
 
 # RockSample Example:
-
-myPOMDP = RockSampleDecisions()
-dnplot(myPOMDP.model)
+myPOMDP = RockSampleDecisionsPOMDP()
+# dnplot(myPOMDP.model)
 # Step 1: 
 myBMDP = myPOMDP |> Unimplement(:r, :o,:mp)
 
 # Step 2: 
 myBMDP = myBMDP |> MergeForward(:s, :sp, :o)
-dnplot(myBMDP.model)
+# dnplot(myBMDP.model)
 
 
 # Step 3: define an updater from memory node m to memory node mp:
 function genObs(rng; m, a)
     # Set s in the environment model to a sample from m, and :a to a.
     s = rand(rng, m)
-    println(s)
-
     sp = rand(myPOMDP[:sp]; s, a)
     o = rand(myPOMDP[:o]; s, a, sp)
     
     return o
 end
 
-# function update( m, a, o)
-#     # Set s in the environment model to a sample from m, and :a to a.
-#     return m
-# end
 
 mem_updater = @ConditionalDist Any begin
     function rand(rng; m, a)
         o = genObs(rng; m,a)
-        println(o)
         rand(myPOMDP[:mp]; m, a, o)
     end
 end
@@ -116,18 +48,17 @@ myBMDP = myBMDP |> Implement(;
 
 # Step 4: recondition r on m, a, mp
 myBMDP = myBMDP |> Recondition(; r =(Dense(:a),Dense(:m),Dense(:mp)))
-dnplot(myBMDP.model)
+# dnplot(myBMDP.model)
 
 # Step 5: Reimplement reward
 calc_reward = @ConditionalDist Float64 begin
     function rand(rng; m, a, mp)
-        Ns = 50
+        Ns = 1000
         r = 0.0
         for i in range(1,Ns)    
             s_i = rand(rng, m)
             sp_i = rand(rng, mp)
             r += rand(myPOMDP[:r]; s = s_i, a, sp = sp_i)
-            println(r)
         end
         return r /=Ns
     end
@@ -141,17 +72,30 @@ myBMDP = myBMDP |> Implement(;
 # Step 6: Rename m--> s, and mp --> sp
 myBMDP = myBMDP |> Rename(; m = :s, mp = :sp)
 myBMDP = myBMDP |> SetNodeHints(:sp; is_terminable=true)
-dnplot(myBMDP.model)
+# dnplot(myBMDP.model)
 
 # Check that it is now an MDP:
 myBMDP isa Decisions.MDP
 
 
-
 s = myBMDP.initial.rand()
 a = rand(Decisions.support(myBMDP[:a]))
-a = rand(Decisions.support(myPOMDP[:a]))
 sp =rand(myBMDP[:sp]; s,a)
 r = rand(myBMDP[:r]; s, a, sp)
 
-# sample or simulate
+s = myBMDP.initial.rand()
+function run_for_N(s, N)
+    n = 0
+    while n<=N
+        a = rand(Decisions.support(myBMDP[:a]))
+        sp =rand(myBMDP[:sp]; s,a)
+        r = rand(myBMDP[:r]; s, a, sp)
+        println(r)
+        s = deepcopy(sp)
+        n +=1
+    end
+    
+end
+
+N = 100
+run_for_N(s,N)
